4f991979d9fa82f5f533f16c1f0225e8ffc4a74b
Boris Sijan <sijanb@outlook.com>
2019-02-26 22:02:07 +0100

    initial

---
+++ MediatRPublishTest/MediatRPublishTest.Console/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MediatRPublishTest.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var services = new ServiceCollection();
+            services.AddScoped<ServiceFactory>(p => p.GetService);
+
+            services.AddSingleton<Pub>()
+        }
+    }
+}
